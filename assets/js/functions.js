591673c8c240714f77ad5dafe8df8f4b5d1f097c
Pablo Noel <hello@pablonoel.com>
2014-09-04 22:55:20 -0400

    First Commit
    SASS Required

---
+++ assets/js/functions.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  var viewportWidth = $(window).width()
+    , viewportHeight = $(window).height()
+
+  $('section.slide').css({'min-height':viewportHeight})
+
+})
---
+++ config.rb
@@ -0,0 +1,19 @@
+# Require any additional compass plugins here.
+
+http_path = "/"
+css_dir = "assets/css/"
+sass_dir = "assets/sass/"
+images_dir = "assets/img/"
+javascripts_dir = "assets/js/"
+
+output_style = :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+line_comments = false
+
+preferred_syntax = :sass
+
+output_style = :compressed # by Compass.app 
+sass_options = {:debug_info=>true} # by Compass.app 
